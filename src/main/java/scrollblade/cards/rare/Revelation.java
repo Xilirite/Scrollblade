485ef3b1d4b2bb029c0cbca057d6b60cd47c9f89
Xilirite <99162031+Xilirite@users.noreply.github.com>
2026-03-12 03:33:44 -0400

    Revelation

---
+++ src/main/java/scrollblade/cards/rare/Revelation.java
@@ -0,0 +1,39 @@
+package scrollblade.cards.rare;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import scrollblade.cards.BaseCard;
+import scrollblade.character.ScrollbladeCharacter;
+import scrollblade.powers.DictumPower;
+import scrollblade.util.CardStats;
+
+public class Revelation extends BaseCard {
+    public static final String ID = makeID("Revelation");
+    private static final CardStats info = new CardStats(
+            ScrollbladeCharacter.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.RARE,
+            CardTarget.SELF,
+            1
+    );
+
+    public Revelation() {
+        super(ID, info);
+        {
+            setExhaust(true);
+            setCostUpgrade(0);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster M) {
+        addToBot(new GainEnergyAction(p.getPower(DictumPower.POWER_ID).amount));
+        addToBot(new DrawCardAction(p.getPower(DictumPower.POWER_ID).amount));
+        flash();
+        addToBot(new RemoveSpecificPowerAction(p, p, DictumPower.POWER_ID));
+    }
+}
